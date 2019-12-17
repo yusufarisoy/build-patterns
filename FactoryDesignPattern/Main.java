2bdea24e17c08eb21c8e9464eefa20859c37cd14
yusufarisoy <53172424+yusufarisoy@users.noreply.github.com>
2019-12-18 01:32:18 +0300

    Update Main.java

--- FactoryDesignPattern/Main.java
+++ FactoryDesignPattern/Main.java
@@ -7,8 +7,8 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		Bilgisayar bilgisayar = BilgisayarFactory.Get_Computer("PC", "16 GB", "1 TB", "2.4 GHz");
-		Bilgisayar server = BilgisayarFactory.Get_Computer("Server", "128 GB", "5 TB", "2.9 GHz");
+		Bilgisayar bilgisayar = BilgisayarFactory.Get_Bilgisayar("PC", "16 GB", "1 TB", "2.4 GHz");
+		Bilgisayar server = BilgisayarFactory.Get_Bilgisayar("Server", "128 GB", "5 TB", "2.9 GHz");
 		
 		System.out.println("Factory PC Config : " + bilgisayar);
 		System.out.println("Factory Server Config : " + server);
